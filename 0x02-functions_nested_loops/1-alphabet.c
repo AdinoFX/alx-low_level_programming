a88e8582c0e559147f261cecbc93012aec77c064
AdinoFX <ubongabasiebebe41@gmail.com>
2023-08-15 09:54:29 -0700

    printing of alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print alphabets in lowercase
+ *
+ * Return: (0)
+ *
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
